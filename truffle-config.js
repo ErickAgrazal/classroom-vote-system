e64946df0571d2961236ad708756194a495935fa
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-06-12 18:24:39 -0500

    added ropsten migration

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = "cart crew runway fog excite almost anxiety income account balcony sound castle";
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -11,7 +16,13 @@ module.exports = {
       network_id: "*", // Match any network id,
       gas: 4612388,
       gasPrice: 25000000000
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/118f1f44a28c478baf0efad23ceaab5f")
+      },
+      network_id: 4
+    },
   },
   solc: {
     optimizer: {
