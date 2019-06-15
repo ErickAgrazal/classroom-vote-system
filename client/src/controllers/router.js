07675dae70e888b92884e5604ff94c6989388a11
Erick Agrazal <erick.agrazal@gmail.com>
2019-06-14 19:16:02 -0500

    Added new panel envs.

--- client/src/controllers/router.js
+++ client/src/controllers/router.js
@@ -6,20 +6,23 @@ const router = express.Router();
 const file = fs.readFileSync(path.join(__dirname, '..', 'contracts/Voting.json'));
 const contract = JSON.parse(file);
 
+const contractAddress = process.env.CONTRACT_ADDRESS;
+console.log(contractAddress);
+
 router.get('/', (req, res) => {
-    res.render('dashboard');
+    res.render('dashboard', { contractAddress });
 });
 
 router.get('/registrarse', (req, res) =>{
-    res.render('register');
+    res.render('register', { contractAddress });
 });
 
 router.get('/votar', async (req, res) => {
-    res.render('vote');
+    res.render('vote', { contractAddress });
 });
 
 router.get('/panel', async (req, res) => {
-    res.render('panel');
+    res.render('panel', { contractAddress });
 });
 
 router.post('/api/contract', (req, res) => {
